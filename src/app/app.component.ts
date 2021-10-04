0db2bfdfe96be692f109c60102306c7d9c383461
Naveen <naveen@neoito.com>
2021-10-04 15:07:52 +0530

    chore: add sample data and import tree grid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tree-grid';
+
+  sampleData =  [{ taskID: 1, taskName: 'Development Task 1', startDate: new Date('02/17/2017'),
+  endDate: new Date('02/20/2017'), duration: 4, progress: '50', priority: 'Normal', approved: true },
+{ taskID: 2, taskName: 'Development Task 2', startDate: new Date('02/17/2017'),
+  endDate: new Date('02/20/2017'), duration: 4, progress: '50', priority: 'Critical', approved: true },
+{ taskID: 3, taskName: 'Testing', startDate: new Date('02/21/2017'),
+  endDate: new Date('02/24/2017'), duration: 2, progress: '0', priority: 'High', approved: false }
+  ]
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { TreeGridModule } from '@syncfusion/ej2-angular-treegrid';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TreeGridModule
   ],
   providers: [],
   bootstrap: [AppComponent]
